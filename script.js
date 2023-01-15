4166f1ff0d225755538db5f56e55ac75d9307b61
LovroG05 <lovro.govekar@gmail.com>
2023-01-15 20:40:27 +0100

    neki ig

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-function onChange (oldPos, newPos) {
-    console.log('Position changed:')
-    console.log('Old position: ' + Chessboard.objToFen(oldPos))
+function onDrop (source, target, piece, newPos, oldPos, orientation) {
+    console.log('Source: ' + source)
+    console.log('Target: ' + target)
+    console.log('Piece: ' + piece)
     console.log('New position: ' + Chessboard.objToFen(newPos))
+    console.log('Old position: ' + Chessboard.objToFen(oldPos))
+    console.log('Orientation: ' + orientation)
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "source": source,
+        "target": target,
+        "piece": piece,
+        "orientation": orientation
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    let uuid = $("#uuid").text();
+
+    fetch("http://localhost:4567/g/"+ uuid +"/move", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            console.log(result);
+            const obj = JSON.parse(result);
+            board1.position(obj.fen);
+        })
+        .catch(error => console.log('error', error));
 }
 
-var config = {
-    position: 'RNBQKBNR/PPPPPPPP/8/8/8/8/pppppppp/rnbqkbnr',
-    draggable: true,
-    onChange: onChange
-}
-  
-  
-var board1 = ChessBoard('board1', config);
-
-  
 function onLoad() {
     fetch("http://localhost:4567/g/new")
     .then(response => response.text())
@@ -25,3 +47,12 @@ function onLoad() {
     })
     .catch(error => console.log('error', error));
 }
+
+var config = {
+    position: 'start',
+    draggable: true,
+    onDrop: onDrop
+}
+  
+  
+var board1 = ChessBoard('board1', config);
