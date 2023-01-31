80ae5730dee4fed67f9469cd352f93e948eb0cac
LovroG05 <lovro.govekar@gmail.com>
2023-01-31 20:02:00 +0100

    pc push

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var webSocket;
+
 function onDrop (source, target, piece, newPos, oldPos, orientation) {
     console.log('Source: ' + source)
     console.log('Target: ' + target)
@@ -17,39 +19,60 @@ function onDrop (source, target, piece, newPos, oldPos, orientation) {
         "orientation": orientation
     });
 
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-
-    let uuid = $("#uuid").text();
-
-    fetch("http://localhost:4567/g/"+ uuid +"/move", requestOptions)
-        .then(response => response.text())
-        .then(result => {
-            console.log(result);
-            const obj = JSON.parse(result);
-            board1.position(obj.fen);
-            $("#next").text(obj.fen.slice(-1));
-            $("#status").text(obj.status);
-        })
-        .catch(error => console.log('error', error));
+    webSocket.send(raw);
 }
 
 function onLoad() {
-    fetch("http://localhost:4567/g/new")
-    .then(response => response.text())
-    .then(result => {
-        console.log(result);
-        const obj = JSON.parse(result);
-        board1.position(obj.fen);
-        $("#uuid").text(obj.uuid);
-        $("#next").text(obj.fen.slice(-1));
-        $("#status").text(obj.status);
-    })
-    .catch(error => console.log('error', error));
+    if (!confirm("Se želiš pridružiti igri?")) {
+        // start new game
+        fetch("http://localhost:4567/g/new")
+            .then(response => response.text())
+            .then(result => {
+                console.log(result);
+                const obj = JSON.parse(result);
+                board1.position(obj.fen);
+                $("#uuid").text(obj.uuid);
+                $("#next").text(obj.fen.slice(-1));
+                $("#status").text(obj.status);
+                document.title = obj.isWhite ? "white": "black";
+
+                webSocket = new WebSocket("ws://localhost:4567/game?uuid="+obj.uuid+"&iswhite="+obj.isWhite);
+                webSocket.onmessage = function (msg) {
+                    console.log(msg.data)
+                    const msgobj = JSON.parse(msg.data);
+                    board1.position(msgobj.fen);
+                    $("#next").text(msgobj.fen.slice(-1));
+                    $("#status").text(msgobj.status);
+                };
+                webSocket.onclose = function () { alert("WebSocket connection closed") };
+            })
+            .catch(error => console.log('error', error));
+    } else {
+        var uuid = prompt("Prosim prilepi UUID");
+        // join game
+        fetch("http://localhost:4567/g/"+uuid+"/join")
+            .then(response => response.text())
+            .then(result => {
+                console.log(result);
+                const obj = JSON.parse(result);
+                board1.position(obj.fen);
+                $("#uuid").text(obj.uuid);
+                $("#next").text(obj.fen.slice(-1));
+                $("#status").text(obj.status);
+                document.title = obj.isWhite ? "white": "black";
+
+                webSocket = new WebSocket("ws://localhost:4567/game?uuid="+obj.uuid+"&iswhite="+obj.isWhite);
+                webSocket.onmessage = function (msg) {
+                    console.log(msg.data)
+                    const msgobj = JSON.parse(msg.data);
+                    board1.position(msgobj.fen);
+                    $("#next").text(msgobj.fen.slice(-1));
+                    $("#status").text(msgobj.status);
+                };
+                webSocket.onclose = function () { alert("WebSocket connection closed") };
+            })
+            .catch(error => console.log('error', error));
+    }
 }
 
 var config = {
@@ -57,6 +80,6 @@ var config = {
     draggable: true,
     onDrop: onDrop
 }
-  
-  
+
+
 var board1 = ChessBoard('board1', config);
